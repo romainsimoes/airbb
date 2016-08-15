ab477c91653de7d747ccbf4ad5b1ba77ab4d3765
romainsimoes <romain.simoes.pro@gmail.com>
2016-08-15 14:41:40 +0200

    Migration

---
+++ app/models/profile.rb
@@ -0,0 +1,5 @@
+class Profile < ApplicationRecord
+
+  has_many :users
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :children
+  has_many :reviews
+  has_many :favorites
+  has_many :bookings
+  belongs_to :profile
 end
